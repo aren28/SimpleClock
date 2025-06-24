faf7e68489e744b3c179d67268481dbd4d8bc9e0
aren28 <romerallenepadolina28@gmail.com>
2025-06-24 18:48:32 +0900

    fetch url change

--- src/SimpleClock.tsx
+++ src/SimpleClock.tsx
@@ -16,7 +16,8 @@ const SimpleClock = () => {
   const fetchTime = async () => {
     try {
       const guessTimezone = await dayjs.tz.guess();
-      const response = await fetch(`${import.meta.env.VITE_API_ENDPOINT + guessTimezone}`);
+      console.log('Guessed Timezone:', `${import.meta.env.VITE_API_ENDPOINT + guessTimezone}`);
+      const response = await fetch(import.meta.env.VITE_API_ENDPOINT + guessTimezone);
       const data = await response.json();
       setTime(dayjs(data.datetime).tz(`${guessTimezone}`));
     } catch (error) {
