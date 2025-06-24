8f44f64b578230f443f0e2e2d2dd7828b55c6c8f
aren28 <romerallenepadolina28@gmail.com>
2025-06-24 18:43:43 +0900

    next to raect library

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import SimpleClock from './SimpleClock'
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import './assets/index.css'
 
 createRoot(document.getElementById('root')!).render(
